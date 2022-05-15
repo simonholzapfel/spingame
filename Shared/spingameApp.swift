c26605d6af4601c491f9f4a35ff0fe0eb74f2a8b
Simon Holzapfel <simon.holzapfel63@gmail.com>
2022-05-15 18:59:49 +0200

    Initial Commit

---
+++ Shared/spingameApp.swift
@@ -0,0 +1,17 @@
+//
+//  spingameApp.swift
+//  Shared
+//
+//  Created by Simon Holzapfel on 15.05.22.
+//
+
+import SwiftUI
+
+@main
+struct spingameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
